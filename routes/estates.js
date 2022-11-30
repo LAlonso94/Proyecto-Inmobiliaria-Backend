f1583ff136fd768cdbfd4678ee64a69723e644d4
alanz420 <lucasalonso.m94@gmail.com>
2022-11-30 14:36:03 -0300

    Agregue la funcionalidad de borrar y editar(esta ultima todavia queda por resolver)

--- routes/estates.js
+++ routes/estates.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const { runValidation } = require("../validators/index");
-const { allEstates, addEstates } = require("../controllers/controllersEstates");
+const {
+  allEstates,
+  addEstates,
+  searchId,
+  editEstate,
+  deleteEstate,
+} = require("../controllers/controllersEstates");
 
 router.get("/inmuebles", allEstates);
+router.get("/publicacion/:id", searchId);
 router.post("/inmuebles/nuevo", addEstates);
+router.put("/inmuebles/editar/:id", editEstate);
+router.delete("/inmuebles/borrar/:id", deleteEstate);
 
 module.exports = router;
