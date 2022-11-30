639cd0ceec44158d767b71c781fb1f102baeffa2
alanz420 <lucasalonso.m94@gmail.com>
2022-11-30 11:20:43 -0300

    Funcionalidad de agregar inmueble nuevo

--- controllers/controllersEstates.js
+++ controllers/controllersEstates.js
@@ -15,3 +15,54 @@ exports.allEstates = (req, res) => {
       res.status(400).json({ error: error.message });
     });
 };
+
+exports.addEstates = async (req, res) => {
+  const {
+    operacion,
+    tipo,
+    dormitorios,
+    baños,
+    metrosTerreno,
+    metrosEdificados,
+    observaciones,
+    descripcion,
+    precio,
+    garage,
+    departamento,
+    zona,
+    domicilio,
+  } = req.body;
+  try {
+    const newAddress = await knex("direcciones")
+      .returning("direccionId")
+      .insert([
+        {
+          departamento: departamento,
+          zona: zona,
+          domicilio: domicilio,
+        },
+      ]);
+    console.log(newAddress);
+    const newEstate = await knex("inmuebles").insert(
+      [
+        {
+          operacion: operacion,
+          tipo: tipo,
+          dormitorios: dormitorios,
+          baños: baños,
+          metrosTerreno: metrosTerreno,
+          metrosEdificados: metrosEdificados,
+          observaciones: observaciones,
+          descripcion: descripcion,
+          precio: precio,
+          garage: garage,
+          direccionId: newAddress[0].direccionId,
+        },
+      ],
+      ["operacion", "tipo", "dormitorios"]
+    );
+    res.json(newEstate);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
--- routes/estates.js
+++ routes/estates.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { runValidation } = require("../validators/index");
-const { allEstates } = require("../controllers/controllersEstates");
+const { allEstates, addEstates } = require("../controllers/controllersEstates");
 
 router.get("/inmuebles", allEstates);
+router.post("/inmuebles/nuevo", addEstates);
 
 module.exports = router;
