a474bf98fd21d86b2ce824b5ca04e1c1470dba99
Mariano Camacho <macadema@gmail.com>
2022-11-30 10:09:49 -0300

    controller de registro de usuario funcionando. ruta de usuario en routes/users.js creada

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const { register } = require("../controllers/controllerUsers");
 const router = express.Router();
 const { runValidation } = require("../validators/index");
+
+//Registro de usuario
+router.post("/register", register);
 
 module.exports = router;
